73a33e4aa29fb0bd4b1d505cb93dab70966a7374
sdsmnc221 <antr.2201@gmail.com>
2022-10-28 14:33:27 +0200

    chore: clean scroll reveal wrapper

--- components/atoms/ScrollRevealWrapper/scroll-reveal-wrapper.vue
+++ components/atoms/ScrollRevealWrapper/scroll-reveal-wrapper.vue
@@ -1,16 +1,5 @@
 <template>
-  <div
-    ref="scroll-reveal"
-    class="scroll-reveal"
-    :class="{
-      '--top': top,
-      '--bottom': bottom,
-      '--left': left,
-      '--right': right,
-      '--scale': scale,
-    }"
-    :style="cssProps"
-  >
+  <div ref="scroll-reveal" class="scroll-reveal">
     <slot> </slot>
   </div>
 </template>
@@ -18,134 +7,33 @@
 <script>
 export default {
   name: 'ScrollRevealWrapper',
-  props: {
-    top: {
-      type: Boolean,
-      default: false,
-    },
-    bottom: {
-      type: Boolean,
-      default: false,
-    },
-    left: {
-      type: Boolean,
-      default: false,
-    },
-    right: {
-      type: Boolean,
-      default: false,
-    },
-    scale: {
-      type: Boolean,
-      default: false,
-    },
-    rootMargin: {
-      type: String,
-      default: '-160px',
-    },
-    delay: {
-      type: String,
-      default: '0s',
-    },
-  },
-  computed: {
-    cssProps() {
-      return {
-        '--delay': this.delay,
-      }
-    },
-  },
   mounted() {
-    [...this.$refs['scroll-reveal'].querySelectorAll('section > *')].forEach(e => this.$gsap.set(e, {autoAlpha: 0, y: 200}));
-    this.scrollTrigger(this.$refs['scroll-reveal']);
+    ;[...this.$refs['scroll-reveal'].querySelectorAll('section > *')].forEach(
+      (e) => this.$gsap.set(e, { autoAlpha: 0, y: 200 })
+    )
+    this.scrollTrigger(this.$refs['scroll-reveal'])
   },
   methods: {
     scrollTrigger(el, options = { rootMargin: this.rootMargin }) {
-      //   let els = document.querySelectorAll(selector)
-      //   els = Array.from(els)
-      //   els.forEach((el) => {
-      //     this.addObserver(el, options)
-      //   })
-      // this.addObserver(el, options)
       const tl = this.$gsap.timeline({
-          scrollTrigger: {
-            trigger: el,
-            start: 'top center',       
-          }
-      });
-
-      [...el.querySelectorAll('section > *')].forEach(e => {
-        tl.to(e, { 
-          autoAlpha: 1,
-          y: 0,
-          ease: 'out.sine',
-        }, '>')
+        scrollTrigger: {
+          trigger: el,
+          start: 'top center',
+        },
       })
-    
-    },
 
-    addObserver(el, options) {
-      if (!('IntersectionObserver' in window)) {
-        if (options.cb) {
-          options.cb(el)
-        } else {
-          //   entry.target.classList.add('active')
-          el.classList.add('active')
-        }
-        return
-      }
-      const observer = new IntersectionObserver((entries, observer) => {
-        // this takes a callback function which receives two arguments: the elemts list and the observer instance
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            if (options.cb) {
-              options.cb(el)
-            } else {
-              entry.target.classList.add('active')
-            }
-            observer.unobserve(entry.target)
-          }
-        })
-      }, options)
-      observer.observe(el)
+      ;[...el.querySelectorAll('section > *')].forEach((e) => {
+        tl.to(
+          e,
+          {
+            autoAlpha: 1,
+            y: 0,
+            ease: 'out.sine',
+          },
+          '>'
+        )
+      })
     },
   },
 }
 </script>
-
-<style lang="scss">
-// .scroll-reveal {
-//   position: relative;
-//   opacity: 0;
-//   transition: all $ease-out-sine 1.2s;
-//   transition-delay: var(--delay);
-
-//   &.--top {
-//     top: 20vh;
-//   }
-
-//   &.--bottom {
-//     top: -80vh;
-//   }
-
-//   &.--left {
-//     left: 20vw;
-//   }
-
-//   &.--right {
-//     right: 20vw;
-//   }
-
-//   &.--scale {
-//     transform: scale(0);
-//   }
-
-//   &.active {
-//     opacity: 1;
-//     transform: scale(1);
-//     top: 0;
-//     left: 0;
-//     right: 0;
-//   }
-// }
-</style>
