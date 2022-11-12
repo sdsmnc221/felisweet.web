98645e1f218fac7ca7e04b6aa4265662d2b75dc7
sdsmnc221 <antr.2201@gmail.com>
2022-11-12 03:36:17 +0100

    feat: style popup

--- components/molecules/PopUp/pop-up.vue
+++ components/molecules/PopUp/pop-up.vue
@@ -52,12 +52,12 @@ export default {
     top: -72px;
     right: -72px;
     @include rem(padding, $spacing-m);
-    background-color: rgba($color-link-water, 64%);
+    background-color: transparentize($color-service-blue, 0.8);
     border-radius: $radius-full;
 
     &:hover {
       &::after {
-        border: 4px dashed $color-link-water;
+        border: 4px dashed $color-shakespear-blue;
         transform: translate(-50%, -50%) scale(1);
       }
     }
@@ -66,7 +66,7 @@ export default {
       z-index: -1;
       display: block !important;
       background-image: none;
-      border: 4px dashed transparent;
+      border: 4px dashed $color-link-water;
       border-radius: $radius-full;
       transform: translate(-50%, -50%) scale(0);
       transition: all ease 0.64s;
@@ -78,10 +78,25 @@ export default {
     height: 100%;
     overflow: scroll;
 
+    * {
+      @include rem(font-size, $font-size-body-l);
+    }
+
+    a {
+      color: $color-shakespear-blue;
+    }
+
     iframe {
       width: 100%;
       height: 100%;
       mix-blend-mode: multiply;
+    }
+
+    h1,
+    h2,
+    h3 {
+      @include rem(font-size, $font-size-heading-2);
+      font-weight: 700;
     }
   }
 
@@ -97,6 +112,20 @@ export default {
     border-radius: 48px;
     width: calc(100% + 24px);
     height: calc(100% + 24px);
+    z-index: -1;
+  }
+
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: -100vw;
+    left: -100vh;
+    width: 400vw;
+    height: 400vh;
+    overflow: hidden;
+    backdrop-filter: blur(12px);
+    background-color: transparentize($color-shakespear-blue, 0.8);
     z-index: -1;
   }
 }
