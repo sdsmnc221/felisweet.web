ab5768dcb74519ee8ae5d9cd1fa0df95e07926d9
sdsmnc221 <antr.2201@gmail.com>
2024-03-23 10:04:02 +0100

    chore(app): rm old

--- pages/slice-simulator.vue
+++ pages/slice-simulator.vue
@@ -7,14 +7,13 @@
 <script>
 import { SliceSimulator } from '@prismicio/slice-simulator-vue'
 import { components } from '~/slices'
-import state from '~~/.slicemachine/libraries-state.json'
 
 export default {
   components: {
     SliceSimulator,
   },
   data() {
-    return { state, components }
+    return { components }
   },
 }
 </script>
