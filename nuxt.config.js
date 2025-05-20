d5c83bb76640b5dc88ed852c6650b690a356bc82
sdsmnc221 <antr.2201@gmail.com>
2025-05-20 22:51:07 +0200

    fix(app): node-fetch-native error

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,6 +54,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/node-fetch-polyfill.js', mode: 'server' },
     '~/plugins/gsap.js',
     '~/plugins/inlinesvg.js',
     '~/plugins/prismic/enhanced-link-serializer',
